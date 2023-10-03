8eb25f5825bf744e883716676f770895cacc9d00
Eyrim <62777325+Eyrim@users.noreply.github.com>
2023-10-03 15:32:55 +0100

    Revert "Ddg3 user page"

--- src/main/java/com/duckduckgoose/app/services/MemberDetailsService.java
+++ src/main/java/com/duckduckgoose/app/services/MemberDetailsService.java
@@ -4,12 +4,10 @@ import com.duckduckgoose.app.models.auth.MemberDetails;
 import com.duckduckgoose.app.models.database.Member;
 import com.duckduckgoose.app.repositories.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MemberDetailsService implements UserDetailsService {
@@ -26,10 +24,7 @@ public class MemberDetailsService implements UserDetailsService {
         Member member = memberRepository.findByUsername(username);
 
         if (member == null) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    "User not found"
-            );
+            throw new UsernameNotFoundException("Could not find member with username");
         }
 
         return new MemberDetails(member);
